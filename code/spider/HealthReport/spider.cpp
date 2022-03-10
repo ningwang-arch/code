e1844aa9573b99d634d81758a84c531c500b9e59
ningwang-arch <60875240+ningwang-arch@users.noreply.github.com>
2022-03-10 20:18:31 +0800

    fix error

--- code/spider/HealthReport/spider.cpp
+++ code/spider/HealthReport/spider.cpp
@@ -9,7 +9,6 @@ Spider::Spider()
     builder.settings_["emitUTF8"] = true;
     m_mailFormatter.parse(mail_template);
     init();
-    // std::cout << default_data << std::endl;
 }
 
 void Spider::handle(UserInfo info) {
@@ -19,24 +18,16 @@ void Spider::handle(UserInfo info) {
     if (rt != -1) {
         if (rt == 0) { bindUserInfo(info.sn, info.idcard); }
 
-        // rt = isNeedCard();
-        // if (rt == 1)
-        //     std::cout << "已完成打卡: " << respData << std::endl;
-        // else if (rt == 0) {
 
         monitorRegister(data);
         std::stringstream ss;
-        // strToJson(, info.data);
         ss << "打卡信息: " << Json::writeString(builder, data) << std::endl;
-        // resp_data = resp_data.erase(0, 2);
-        // resp_data.pop_back();
         Json::Value root;
         strToJson(respData, root);
         root["data"] = base64_decode(root.get("data", "").asString());
         ss << "打卡结果: " << Json::writeString(builder, root) << std::endl;
-        // std::cout << ss.str() << std::endl;
+        std::cout << ss.str() << std::endl;
         sendEmail(info.mail, ss.str());
-        // }
         cancelBind();
     }
 }
@@ -98,46 +89,6 @@ bool Spider::strToJson(const std::string& body, Json::Value& root) {
     return true;
 }
 
-// std::string Spider::base64_encode(const std::string& in) {
-//     std::string out;
-
-//     int val = 0, valb = -6;
-//     for (auto c : in) {
-//         val = (val << 8) + c;
-//         valb += 8;
-//         while (valb >= 0) {
-//             out.push_back(
-//                 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb)
-//                 &
-//                                                                                    0x3F]);
-//             valb -= 6;
-//         }
-//     }
-//     if (valb > -6)
-//         out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-//                           [((val << 8) >> (valb + 8)) & 0x3F]);
-//     while (out.size() % 4) out.push_back('=');
-//     return out;
-// }
-// std::string Spider::base64_decode(const std::string& in) {
-//     std::string out;
-
-//     std::vector<int> T(256, -1);
-//     for (int i = 0; i < 64; i++)
-//         T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
-
-//     int val = 0, valb = -8;
-//     for (auto c : in) {
-//         if (T[c] == -1) break;
-//         val = (val << 6) + T[c];
-//         valb += 6;
-//         if (valb >= 0) {
-//             out.push_back(char((val >> valb) & 0xFF));
-//             valb -= 8;
-//         }
-//     }
-//     return out;
-// }
 size_t getData(char* ptr, size_t size, size_t num, std::string* stream) {
     if (stream == NULL) return 0;
     stream->append(ptr, size * num);
@@ -221,8 +172,8 @@ int Spider::isNeedCard() {
 }
 
 void Spider::sendEmail(const std::string& to, const std::string& msg) {
-if(to.empty()) return;    
-CURLcode res = CURLE_OK;
+    if (to.empty() || server.empty() || username.empty() || passwd.empty()) return;
+    CURLcode res = CURLE_OK;
     struct curl_slist* recipients = NULL;
     struct upload_status upload_ctx = {0};
 
@@ -257,18 +208,17 @@ int Spider::monitorRegister(Json::Value data) {
 
     std::string e_data = base64_encode(Json::writeString(builder, data));
     curl_easy_setopt(m_curl, CURLOPT_POSTFIELDS, e_data.c_str());
-    // TODO resend when error
     int status = 1;
-    int cnt=0;
-    do{    
+    int cnt = 0;
+    do {
         respData.clear();
         CURLcode res = curl_easy_perform(m_curl);
         Json::Value root;
-        strToJson(respData,root);
-        status = root.get("status",0).asInt();
+        strToJson(respData, root);
+        status = root.get("status", 0).asInt();
         cnt++;
-        if(cnt>=10){break;}
-    }while(!status);
+        if (cnt >= 10) { break; }
+    } while (!status);
     return 0;
 }
 
@@ -289,9 +239,9 @@ Config::Config(std::string filename) {
     if (reader.parse(ifs, root)) {
 
         Json::Value sender = root["sender"];
-	    server = root.get("smtp_server","").asString();
-        username = sender.get("username","").asString();
-        passwd = sender.get("passwd","").asString();
+        server = root.get("smtp_server", "").asString();
+        username = sender.get("username", "").asString();
+        passwd = sender.get("passwd", "").asString();
 
         default_data = root["default_data"];
         Json::Value users = root["users"];
@@ -306,8 +256,6 @@ Config::Config(std::string filename) {
     }
 }
 
-Config::~Config(){
-    if(!m_infos.empty()){
-        m_infos.clear();
-    }
+Config::~Config() {
+    if (!m_infos.empty()) { m_infos.clear(); }
 }
